e9d2f61d22a8f2078d65cef9c1c2c5289773c9bd
Kainz Dominik <dominik.kainz@edu.fh-joanneum.at>
2021-10-29 13:27:42 +0200

    first commit

---
+++ stack_classes.py
@@ -0,0 +1,33 @@
+
+class Stack:
+    def __init__(self):
+        self.numbers = []
+        self.current_size: int = 0
+    
+    def __str__(self) -> str:
+        return f"Stack with {self.current_size} object(s)" 
+    
+    def size(self) -> int:
+        return self.current_size
+
+    def pop(self) -> int:
+        if self.current_size == 0:
+            raise ValueError
+        self.current_size -= 1
+        return self.numbers.pop()
+
+class PositiveNumberStack(Stack):
+    
+    def push(self, n) -> None:
+        if n < 0 or self.current_size == 3:
+            raise ValueError
+        self.current_size += 1        
+        self.numbers.append(n)
+
+class NegativeNumberStack(Stack):
+
+    def push(self, n) -> None:
+        if n > 0 or self.current_size == 3:
+            raise ValueError
+        self.current_size += 1
+        self.numbers.append(n)
